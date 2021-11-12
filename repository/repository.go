aab1018a27964d1a33cf7d4ac3a23e5aa2a0488f
Ayşegül Arpacık <aysegul.arpacik@modanisa.com>
2021-11-12 08:30:31 +0300

    deploy

--- repository/repository.go
+++ repository/repository.go
@@ -3,7 +3,7 @@ package repository
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -48,7 +48,7 @@ func (k *KeyValueStore) LoadKeyValueStoreToMemory(file string) error {
 	fmt.Printf("Successfully Opened '%s'", file)
 	defer jsonFile.Close()
 
-	byteValue, _ := io.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	err = json.Unmarshal(byteValue, &k.store)
 	if err != nil {
